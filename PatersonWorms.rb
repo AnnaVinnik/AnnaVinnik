b7cf0a1e268daca7381e286a7a6a397be3f642f2
Anna <vinnik.anna@rambler.ru>
2020-03-27 20:17:35 +0700

    находит следующее положение и заполняет пути

--- PatersonWorms.rb
+++ PatersonWorms.rb
@@ -2,10 +2,11 @@ class Worms
 	def init
 		@last_position = [0, 0]
 		@next_position = [0, 0]
-		@arm = Array.new(3) { Array.new(4) { Array.new(5) }}
+		@direction = 0
+		@path = Array.new(3) { Array.new(4) { Array.new(5) }}
 		@x = 0
 		@y = 0
-		#p @arm
+		#p @path
 
 	end
 
@@ -44,17 +45,26 @@ class Worms
 	end
 
 	def find_direction(new_direction)
+		@direction = (@direction + new_direction) % 6
+		puts @direction
+	end
 
+	def path_filling
+		@path[@last_position[0]][@last_position[1]][@direction] = 1
+		@path[@next_position[0]][@next_position[1]][(@direction + 3) % 6] = 1
+		p @last_position
+		p @next_position
 	end
 
 	def move(direction = 0)
-		if direction == 0
-			#@position = @position
-		end
-
+		find_direction(5)
+		find_next_position
+		path_filling
+		p @path
 	end
 
 end
 
 one = Worms.new
 one.init
+one.move
